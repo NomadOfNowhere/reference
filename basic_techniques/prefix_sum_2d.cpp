ce06d5a2dbd122cefcb4a52594fa5dc9608db3bc
NomadOfNowhere <joshua10122000@gmail.com>
2025-09-15 14:33:14 -0600

    Updated prefix sum 2D

--- basic_techniques/prefix_sum_2d.cpp
+++ basic_techniques/prefix_sum_2d.cpp
@@ -1,8 +1,11 @@
 // Compute prefix sum 2D
 rep(i,1,n+1) rep(j,1,m+1) 
-    prefix[i][j] = prefix[i-1][j] + prefix[i][j-1]
-                                  - prefix[i-1][j-1]
+    prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] 
+                                  - prefix[i-1][j-1] 
                                   + matrix[i-1][j-1]
 
 // Query prefix sum 2D
-res = prefix[i][j] - prefix[i-1][j] - prefix[i][j-1] + prefix[i-1][j-1]
+query = prefix[i][j] - prefix[i-1][j] - prefix[i][j-1] + prefix[i-1][j-1]
+
+// Specific parts
+query = prefix[r2][c2] - prefix[r1-1][c2] - prefix[r2][c1-1] + prefix[r1-1][c1-1];
