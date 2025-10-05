ae7ca972783b1efe42925977e8efebc2cf3d2ce9
NomadOfNowhere <joshua10122000@gmail.com>
2025-10-05 15:25:41 -0600

    Added cht, dynamic cht and lcp using hashing

--- graphs/kuhn_matching.cpp
+++ graphs/kuhn_matching.cpp
@@ -1,16 +1,16 @@
-vector<int> g[MAXN]; // [0,n)->[0,m)
-int n,m;
-int mat[MAXM];bool vis[MAXN];
+vector<int> adj[N]; // [0,n)->[0,m)
+int n, m, mat[M];
+bool vis[N];
 int match(int x){
-	if(vis[x])return 0;
-	vis[x]=true;
-	for(int y:g[x]) if(mat[y]<0||match(mat[y])) {mat[y]=x;return 1;}
+	if(vis[x]) return 0;
+	vis[x] = 1;
+	for(auto y : adj[x]) if(mat[y]<0 || match(mat[y])) {mat[y]=x; return 1;}
 	return 0;
 }
-vector<pair<int,int> > max_matching(){
-	vector<pair<int,int> > r;
-	memset(mat,-1,sizeof(mat));
-	rep(i,0,n) memset(vis,false,sizeof(vis)),match(i);
-	rep(i,0,m) if(mat[i]>=0)r.pb({mat[i],i});
+vector<pair<int,int>> max_matching(){
+	vector<pair<int, int>> r;
+	memset(mat, -1, sizeof(mat));
+	for(int i=0; i<n; i++) memset(vis, false, sizeof(vis)), match(i);
+	for(int i=0; i<m; i++) if(mat[i] >= 0) r.pb({mat[i], i});
 	return r;
 }
